108707f4bb0c46b7ebfa106ac836cc33916bf365
Mohsporty <m.alassdi@gmail.com>
2024-04-25 03:33:03 +0300

    add the barcode  and time and date also resize the ticket

--- MovieBooking/Cards/Ticket.swift
+++ MovieBooking/Cards/Ticket.swift
@@ -12,6 +12,7 @@ struct Ticket: View {
     @State var title = "Thor"
     @State var subtitle = "Love and Thunder"
     @State var top = "thor-top"
+    @State var bottom = "thor-bottom"
     
     var gradient = [ Color("cyan"), Color("cyan").opacity(0), Color("cyan").opacity(0)]
     var body: some View {
@@ -48,7 +49,71 @@ struct Ticket: View {
                     .stroke(LinearGradient(colors: gradient, startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 2))
             }
             
-            .cornerRadius(40 )
+            .cornerRadius(40, corners: [.topLeft, .topRight])
+            
+            
+            // this code for line under the ticket photo
+            VStack(spacing: 10.0) {
+                Line()
+                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [7]))
+                    .frame(width: 200, height: 1)
+                    .opacity(0.6)
+                
+                HStack(spacing: 20.0){
+                    HStack(spacing: 4.0){
+                        Text("Date:")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        Text("April 24")
+                            .foregroundColor(.black)
+                        
+                    }
+                    HStack(spacing: 4.0){
+                        Text("Time:")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        Text("6 PM")
+                            .foregroundColor(.black)
+                        
+                    }
+                }
+                HStack(spacing: 20.0){
+                    HStack(spacing: 4.0){
+                        Text("Row:")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        Text("2")
+                            .foregroundColor(.black)
+                        
+                    }
+                    HStack(spacing: 4.0){
+                        Text("Seats")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        Text("9, 10")
+                            .foregroundColor(.black)
+                        
+                    }
+                }
+                
+                
+                // bacode photo is here code also the fream is under for shadwo
+                
+                Image("code")
+            }
+            .frame(width: 250, height: 135, alignment: .top)
+            .background(.ultraThinMaterial)
+            .background(Image(bottom)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            )
+            .mask(
+                Image(bottom)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            )
+            
+            
         }
         .frame(height: 460)
         .font(.footnote)
