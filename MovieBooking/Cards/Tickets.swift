f85dce18302470dfd8e859170994c59598f9ce83
Mohsporty <m.alassdi@gmail.com>
2024-04-28 00:18:47 +0300

    this update to add the new page for multipel ticketes this all fetuer on it

---
+++ MovieBooking/Cards/Tickets.swift
@@ -0,0 +1,18 @@
+//
+//  Tickets.swift
+//  MovieBooking
+//
+//  Created by Mohammad  on 27/04/2024.
+//
+
+import SwiftUI
+
+struct Tickets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tickets()
+}
