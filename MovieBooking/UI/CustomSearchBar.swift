fd16fd2c1187505efa816f4f4b16252222fc4617
Mohsporty <m.alassdi@gmail.com>
2024-05-02 19:11:30 +0300

    add the home view page to the first page with all comment and code

---
+++ MovieBooking/UI/CustomSearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSearchBar.swift
+//  MovieBooking
+//
+//  Created by Mohammad  on 02/05/2024.
+//
+
+import SwiftUI
+
+struct CustomSearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSearchBar()
+}
---
+++ MovieBooking/UI/ScrollSection.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollSection.swift
+//  MovieBooking
+//
+//  Created by Mohammad  on 02/05/2024.
+//
+
+import SwiftUI
+
+struct ScrollSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollSection()
+}
