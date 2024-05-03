eda3a9afe3c6e642394944db71623145c946cb90
Mohsporty <m.alassdi@gmail.com>
2024-05-03 03:02:13 +0300

    add 2 page is booking view is done lso add the seats page 2 featuer and fixed 1 bugs

---
+++ MovieBooking/Views/SeatsView.swift
@@ -0,0 +1,18 @@
+//
+//  SeatsView.swift
+//  MovieBooking
+//
+//  Created by Mohammad  on 03/05/2024.
+//
+
+import SwiftUI
+
+struct SeatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeatsView()
+}
